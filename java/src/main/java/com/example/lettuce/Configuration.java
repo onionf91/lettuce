d1687616f2be635b034898dd1549e794de917974
Jason Yu <jason.yu@fuhu.com>
2017-09-26 15:08:41 +0800

    Add spring-boot packages

---
+++ java/src/main/java/com/example/lettuce/Configuration.java
@@ -0,0 +1,16 @@
+package com.example.lettuce;
+
+import javax.annotation.PostConstruct;
+
+import static com.example.lettuce.EngineLibrary.*;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+
+    @PostConstruct
+    void postConstruct() {
+        Callback cb = new CallbackImpl();
+        Engine engine = new Engine(cb);
+        engine.run();
+    }
+}
